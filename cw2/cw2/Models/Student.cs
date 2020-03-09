39be836a159ed792223c057a5a6cf13c738416e1
s18732 <s18732@pjwstk.edu.pl>
2020-03-09 17:12:25 +0100

    dopisanie paru linijek

--- cw2/cw2/Models/Student.cs
+++ cw2/cw2/Models/Student.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace cw2.Models
 {
-    class Student
+    public class Student
     {
-        public string Email { get; set; }
+        //[XmlAttribute(attributeName:"email")]
+       // public string Email { get; set; }
+        [XmlElement(elementName: "fname")]
         public string Imie { get; set; }
+        [XmlElement(elementName: "lname")]
         public string Nazwisko { get; set; }
+        [XmlElement(elementName: "birthdate")]
+        public string DataUrodzenia { get; set; }
+        [XmlElement(elementName: "email")]
+        public string Email { get; set; }
+        [XmlElement(elementName: "mothersName")]
+        public string ImieMatki { get; set; }
+        [XmlElement(elementName: "fathersName")]
+        public string ImieOjca { get; set; }
+        //[XmlAttribute(attributeName: "studies")]
+        [XmlElement(elementName: "name")]
+        public string KierunekStudiow { get; set; }
+        [XmlElement(elementName: "mode")]
+        public string TypStudiow { get; set; }
+
+
     }
 }
--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace cw2
 {
@@ -9,21 +10,40 @@ namespace cw2
     {
         static void Main(string[] args)
         {
+            //string path = "data.csv";
             string path = "Data\\dane.csv";
+            string outPath = "result.xml";
+            string dataType = "xml";
+            if (args.Length > 3)
+            {
+                Console.WriteLine("Za duzo argumentow");
+                Environment.Exit(-1);
+            }
+            else if (args.Length == 3)
+            {
+                path = args[0];
+                outPath = args[1];
+                dataType = args[2];
+            }
+            var list = new List<Student>();
 
             //Wczytywanie
             var fi = new FileInfo(path);
-            using (var stream = new StreamReader(fi.OpenRead()))
+           /* using (var stream = new StreamReader(fi.OpenRead()))
             {
                 string line = null;
                 while ((line = stream.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    string[] kolumny = line.Split(',');
+                    list.Add(new Student
+                    {
+
+                    });
                 }
-            }
+            }*/
 
             //XML
-            var list = new List<Student>();
+            //var list = new List<Student>();
             var st = new Student
             {
                 Imie = "Jan",
@@ -31,6 +51,11 @@ namespace cw2
                 Email = "kowalski@wp.pl"
             };
             list.Add(st);
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+
+            serializer.Serialize(writer, list);
 
         }
 
