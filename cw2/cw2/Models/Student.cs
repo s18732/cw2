dff18a57c4dddd1e61993d52e223d4d9d3ae5529
s18732 <s18732@pjwstk.edu.pl>
2020-03-09 16:31:50 +0100

    zaczecie zadania

---
+++ cw2/cw2/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2.Models
+{
+    class Student
+    {
+        public string Email { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
---
+++ cw2/cw2/Program.cs
@@ -0,0 +1,38 @@
+﻿using cw2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Data\\dane.csv";
+
+            //Wczytywanie
+            var fi = new FileInfo(path);
+            using (var stream = new StreamReader(fi.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            //XML
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@wp.pl"
+            };
+            list.Add(st);
+
+        }
+
+    }
+}
