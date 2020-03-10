88e94de8fc5411d107063fe61959ab57f0b2bbe5
s18732 <s18732@pjwstk.edu.pl>
2020-03-10 13:58:49 +0100

    zmiany

---
+++ cw2/cw2/Models/Studia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace cw2.Models
+{
+    [XmlTypeAttribute(typeName: "studies")]
+    public class Studia
+    {
+        [JsonPropertyName("name")]
+        [XmlElement(elementName: "name")]
+        public string Kierunek { get; set; }
+        [JsonPropertyName("mode")]
+        [XmlElement(elementName: "mode")]
+        public string Typ { get; set; }
+    }
+}
--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace cw2
 {
@@ -28,35 +30,75 @@ namespace cw2
             var list = new List<Student>();
 
             //Wczytywanie
-            var fi = new FileInfo(path);
-           /* using (var stream = new StreamReader(fi.OpenRead()))
+            try
             {
-                string line = null;
-                while ((line = stream.ReadLine()) != null)
-                {
-                    string[] kolumny = line.Split(',');
-                    list.Add(new Student
-                    {
+                var fi = new FileInfo(path);
+                 using (var stream = new StreamReader(fi.OpenRead()))
+                 {
+                     string line = null;
+                     while ((line = stream.ReadLine()) != null)
+                     {
+                        string[] kolumny = line.Split(',');
+                        if(kolumny.Length == 9)
+                        {
+                            bool puste = false;
+                            for(int i = 0; i < 9; i++)
+                            {
+                                if(kolumny[i].Equals("") || kolumny[i].Equals(" "))
+                                {
+                                    //dodac studenta do log.txt
+                                    puste = true;
+                                }
+                            }
+                            if (!puste)
+                            {
+                                //Student stud = new Student(kolumny[4],kolumny[0],kolumny[1],kolumny[5],kolumny[6],kolumny[7],kolumny[8],kolumny[2],kolumny[3]);
+                                Studia studia = new Studia
+                                {
+                                    Kierunek = kolumny[2],
+                                    Typ = kolumny[3]
+                                };
+                                Student stud = new Student
+                                {
+                                    Indeks = kolumny[4],
+                                    Imie = kolumny[0],
+                                    Nazwisko = kolumny[1],
+                                    DataUrodzenia = kolumny[5],
+                                    Email = kolumny[6],
+                                    ImieMatki = kolumny[7],
+                                    ImieOjca = kolumny[8],
+                                    //KierunekStudiow = kolumny[2],
+                                    //TypStudiow = kolumny[3]
+                                    Studia = studia
+                                };
+                                if (!list.Contains(stud))
+                                    list.Add(stud);
+                            }  
+                        }
+                        else
+                        {
+                            //zapisac blad do log.txt
+                        }
+                     }
+                 }
 
-                    });
-                }
-            }*/
+                var jsonString = JsonSerializer.Serialize(list, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText("data.json", jsonString);
 
-            //XML
-            //var list = new List<Student>();
-            var st = new Student
+                FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+
+                serializer.Serialize(writer, list);
+            }catch(ArgumentException e)
             {
-                Imie = "Jan",
-                Nazwisko = "Kowalski",
-                Email = "kowalski@wp.pl"
-            };
-            list.Add(st);
-
-            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
-
-            serializer.Serialize(writer, list);
-
+                //zapisac blad do log.txt
+                throw new ArgumentException("Podana sciezka jest niepoprawna");
+            }catch(FileNotFoundException e)
+            {
+                //zapisac blad do log.txt
+                throw new FileNotFoundException("Plik nazwa nie istnieje");
+            }
+            
         }
 
     }
